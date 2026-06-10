a46220e24181026730fa27542f3c0a4c5fd9c437
Alexey Postnov <adpostnov@yandex.ru>
2026-06-11 00:19:57 +0500

    added notification service

--- NotificationService/src/main/kotlin/main.kt
+++ NotificationService/src/main/kotlin/main.kt
@@ -1,8 +1,35 @@
 package ru.sagenotes.notificationservice
 
-import io.ktor.server.engine.*
 import io.ktor.server.application.*
+import io.ktor.server.websocket.*
+import org.koin.ktor.ext.inject
+import org.koin.ktor.plugin.Koin
+import ru.sagenotes.notificationservice.data.di.appModule
+import ru.sagenotes.notificationservice.presentation.consumer.RabbitMqConsumer
+import ru.sagenotes.notificationservice.presentation.route.resfull.configureNotificationRouting
+import kotlin.time.Duration.Companion.seconds
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    install(WebSockets) {
+        pingPeriod = 30.seconds
+        timeout = 15.seconds
+    }
+
+    install(Koin) {
+        modules(
+            appModule
+        )
+    }
+
+    configureSerialization()
+    configureSecurity()
+
+    configureNotificationRouting()
+
+    val rabbitMqConsumer: RabbitMqConsumer by inject()
+    rabbitMqConsumer.start()
 }
